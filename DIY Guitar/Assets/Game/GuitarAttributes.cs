113e6bd3a6eda7c7b221cbc04040a0b92e7d5602
branima <thebranima@gmail.com>
2022-05-04 14:17:28 +0200

    ready for upgrades

---
+++ DIY Guitar/Assets/Game/GuitarAttributes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PaintIn3D.Examples;
+using PaintIn3D;
+
+public class GuitarAttributes : MonoBehaviour
+{
+
+    public Vector3 cleaningPosition;
+    public Vector3 cleaningRotation;
+    public float cleaningSize;
+
+    public float fillBarScaler;
+    public P3dChannelCounterFill fillScript;
+
+    public BrushesAndPropsLogic brushesAndProps;
+
+    public float propsAndBrushesModifier;
+
+    public string guitarType;
+
+    void Start()
+    {
+        fillScript.fillBarScaler = fillBarScaler;
+        brushesAndProps.ScaleBrushesAndProps(propsAndBrushesModifier);
+    }
+
+    public void ResetBrushesAndProps()
+    {
+        brushesAndProps.ResetBrushesAndProps(propsAndBrushesModifier);
+    }
+
+    public Vector3 GetCleaningPosition()
+    {
+        return cleaningPosition;
+    }
+
+    public Vector3 GetCleaningRotation()
+    {
+        return cleaningRotation;
+    }
+
+    public float GetCleaningSize()
+    {
+        return cleaningSize;
+    }
+}
